20ac0c6158d3e040394464774e6b7b826d4db01b
Branden <bsmceachern@gmail.com>
2021-08-16 15:49:22 -0500

    new .jar file now accepts 2 args and prints out the name and email address supplied

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -3,7 +3,12 @@ package com.statefarm;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        if (args.length == 0) {
+            System.out.println("Please specify a name and email");
+        } else if
+            (args.length == 1) {
+            System.out.println("Please specify an email for " + args[0]);
+        } else
+            System.out.println(args[0] + " <" + args[1] + ">" );
     }
-
 }
